55078ef35c212ba7889040dc0e96934f96ca5de5
NicksyRop <nicksonkipkorir25@gmail.com>
2022-03-05 09:13:04 +0300

    added the auth route and encrpted user passwords

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
 
 const dontenv = require("dotenv");
 dontenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Database connected successfully"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRouter);
 
 app.listen(5000, () => {
   console.log(`Server runnimg on http://localhost:5000`);
