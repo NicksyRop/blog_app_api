50f6957ec9d40ea096b3e8cebe31bc3764aa28b7
NicksyRop <nicksonkipkorir25@gmail.com>
2022-03-05 08:08:11 +0300

    added User , Post and Category models schemas

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      required: true,
+      type: String,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
